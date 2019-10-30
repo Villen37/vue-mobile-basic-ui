ea84802ac0bf6a1d3c0ddcb08858599b93623b72
wanglu <wanglu@happyjuzi.com>
2019-10-30 17:58:41 +0800

    index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
  * Created by villen on 2018/10/24.
  */
 import Toast from './packages/toast/index.js';
+import PullDownRefresh from './packages/pull-down-refresh/index.js';
 
 
 const components = {
     Toast,
+    PullDownRefresh
 };
 
 const install = function (Vue, opts = {}) {
